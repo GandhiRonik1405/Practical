1345de4a2442f4acc67461744794e9a22f6eddcd
GandhiRonik1405 <21amtics032@gmail.com>
2025-07-13 11:23:22 +0530

    Initial commit of working Terraform project for Medusa ECS deployment

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,14 @@
 resource "aws_ecr_repository" "medusa_repo" {
-  name = "medusa-backend"
+  name                 = "medusa"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "medusa"
+    Environment = "dev"
+  }
 }
+
